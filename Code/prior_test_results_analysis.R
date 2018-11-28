77f1cd03097bde867a1f6985eaad0a85f4b486e7
Ian Taylor <ian.m.taylor1@gmail.com>
2018-11-28 15:03:24 -0700

    Updated color palette in results analysis.

--- Code/prior_test_results_analysis.R
+++ Code/prior_test_results_analysis.R
@@ -1,6 +1,14 @@
 # Analyze the results of the first Simulation using the custom version of the code
+library(RColorBrewer)
 
-results <- read.csv("Results/priortest_results.csv", header=TRUE, stringsAsFactors=FALSE)
+mypalette <- c(
+  brewer.pal(10,"Paired")[c(1,3,5,9)]
+)
+
+palette(mypalette)
+
+
+results <- read.csv("Results/Global_Horseshoe_prior_results.csv", header=TRUE, stringsAsFactors=FALSE)
 
 results[,"Covered"] <- 1 * ((results[,"TrueValue"] >= results[,"CI_low"]) & (results[,"TrueValue"] <= results[,"CI_high"]))
 results[,"CI_width"] <- results[,"CI_high"] - results[,"CI_low"]
@@ -40,55 +48,55 @@ pdf('rplot.pdf', width=10.5, height=8.5)
 
 # Plot coverage of confidence intervals
 boxplot(coverage~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data with Additive Unobserved Covariates", ylab="Coverage")
 abline(h=0.9,col="Black",lty=2) # HARD CODED
 boxplot(coverage~Var*RndEffects, data=aggregate[(aggregate[,"Z_multiplicative"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data With Multiplicative Unobserved Covariate", ylab="Coverage")
 abline(h=0.9,col="Black",lty=2) # HARD CODED
 boxplot(coverage~Var*RndEffects, data=aggregate[(aggregate[,"Z_multiplicative"]==FALSE)&(aggregate[,"Z_additive"]==FALSE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data Without Unobserved Covariates", ylab="Coverage")
 abline(h=0.9,col="Black",lty=2) # HARD CODED
 
 # Plot means of estimates over all Reps
 boxplot(estimate_mean~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data with Additive Unobserved Covariates", ylab="Mean of Estimated Coefficients")
 abline(h=1,col="Black",lty=2) # HARD CODED
 abline(h=-1,col="Black",lty=2) # HARD CODED
 boxplot(estimate_mean~Var*RndEffects, data=aggregate[(aggregate[,"Z_multiplicative"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data WIith Multiplicative Unobserved Covariate", ylab="Mean of Estimated Coefficients")
 abline(h=1,col="Black",lty=2) # HARD CODED
 abline(h=-1,col="Black",lty=2) # HARD CODED
 boxplot(estimate_mean~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==FALSE)&(aggregate[,"Z_multiplicative"]==FALSE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data without Unobserved Covariates", ylab="Mean of Estimated Coefficients")
 abline(h=1,col="Black",lty=2) # HARD CODED
 abline(h=-1,col="Black",lty=2) # HARD CODED
 
 # Plot variances of estimates over all Reps
 boxplot(estimate_var~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data with Additive Unobserved Covariates", ylab="Variance of Estimated Coefficients")
 boxplot(estimate_var~Var*RndEffects, data=aggregate[(aggregate[,"Z_multiplicative"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data With Multiplicative Unobserved Covariate", ylab="Variance of Estimated Coefficients")
 boxplot(estimate_var~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==FALSE)&(aggregate[,"Z_multiplicative"]==FALSE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data without Unobserved Covariate", ylab="Variance of Estimated Coefficients")
 
 # Plot average CI width over all Reps
 boxplot(mean_CI_width~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data with Additive Unobserved Covariates", ylab="Average width of Confidence Interval")
 boxplot(mean_CI_width~Var*RndEffects, data=aggregate[(aggregate[,"Z_multiplicative"]==TRUE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data With Multiplicative Unobserved Covariate", ylab="Average width of Confidence Interval")
 boxplot(mean_CI_width~Var*RndEffects, data=aggregate[(aggregate[,"Z_additive"]==FALSE)&(aggregate[,"Z_multiplicative"]==FALSE),],
-        col=c("Gray","Red","Green","Blue"),
+        col=c(1,2,3,4),
         main="Data without Unobserved Covariates", ylab="Average width of Confidence Interval")
 
 dev.off()
