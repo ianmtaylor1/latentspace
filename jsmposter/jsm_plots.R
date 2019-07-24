7f2610b362989a0f1f6de4d2a276575458691762
Ian Taylor <ian.taylor@colostate.edu>
2019-07-23 22:32:41 -0600

    Added projection results.

--- jsmposter/jsm_plots.R
+++ jsmposter/jsm_plots.R
@@ -19,7 +19,7 @@ results <- read.csv(infile, header=TRUE, stringsAsFactors=FALSE)
 results[,"Covered"] <- 1 * ((results[,"TrueValue"] >= results[,"CI_low"]) & (results[,"TrueValue"] <= results[,"CI_high"]))
 results[,"CI_width"] <- results[,"CI_high"] - results[,"CI_low"]
 
-aggregate <- unique(results[,c("Run","Xseed","Z_additive","Z_multiplicative","AddRE","Variable","TrueValue","prior")])
+aggregate <- unique(results[,c("Run","Xseed","Z_additive","Z_multiplicative","AddRE","Variable","TrueValue","prior","projected")])
 
 for (r in 1:(dim(aggregate)[1])) {
   subresults <- results[(results[,"Run"] == aggregate[r,"Run"]) # Xseed is based on Run
@@ -27,7 +27,8 @@ for (r in 1:(dim(aggregate)[1])) {
                         & (results[,"Z_multiplicative"] == aggregate[r,"Z_multiplicative"])
                         & (results[,"AddRE"] == aggregate[r,"AddRE"])
                         & (results[,"Variable"] == aggregate[r,"Variable"]) # TrueValue is based on Variable
-                        & (results[,"prior"] == aggregate[r,"prior"]),]
+                        & (results[,"prior"] == aggregate[r,"prior"])
+			& (results[,"projected"] == aggregate[r,"projected"]),]
   aggregate[r,"n_runs"] <- dim(subresults)[1]
   aggregate[r,"n_covered"] <- sum(subresults[,"Covered"])
   aggregate[r,"estimate_mean"] <- mean(subresults[,"Estimate"])
@@ -38,90 +39,74 @@ for (r in 1:(dim(aggregate)[1])) {
   } else {
     aggregate[r,"RndEffects"] <- paste("A",aggregate[r,"prior"],sep="_")
   }
-  aggregate[r,"Var"] <- paste("\\beta_",substr(aggregate[r,"Variable"],2,2),sep="")
+  aggregate[r,"Var"] <- ifelse(aggregate[r,"Variable"] == "intercept", "\\beta_0", paste("\\beta_",substr(aggregate[r,"Variable"],2,2),sep=""))
 }
 
 aggregate[,"coverage"] <- aggregate[,"n_covered"] / aggregate[,"n_runs"]
 aggregate[,"estimage_sd"] <- sqrt(aggregate[,"estimate_var"])
+aggregate$RndEffects <- factor(aggregate$RndEffects, levels=c("None","A_IG","A_HC"))
+aggregate$projected <- factor(aggregate$projected, levels=c(FALSE, TRUE))
+
+
 
 ###############################################################################
 
 pdf(outfile, width=9, height=6)
 
-# Coverage for data with no hidden covariates and inverse-gammma prior
-ggplot(
-  subset(aggregate, (Z_additive == FALSE)&(Z_multiplicative==FALSE)&(RndEffects=="A_IG")),
-  aes(x=Variable, y=coverage)
-  ) +
-  geom_boxplot(aes(col=Variable)) + guides(color=FALSE) +
-  coord_cartesian(ylim=c(0.5,1)) +
-  geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="No Nodal Variation, Inverse-Gamma",
-       x="", y="Coverage") +
-  theme(text=element_text(size=24))
 
-# Coverage for data with no hidden covariates and half-cauchy prior
+# Coverage for when there is no nodal variation - priors
 ggplot(
-  subset(aggregate, (Z_additive == FALSE)&(Z_multiplicative==FALSE)&(RndEffects=="A_HC")),
-  aes(x=Variable, y=coverage)
-) +
-  geom_boxplot(aes(col=Variable)) + guides(color=FALSE) +
-  coord_cartesian(ylim=c(0.5,1)) +
-  geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="No Nodal Variation, Half-Cauchy",
-       x="", y="Coverage") +
-  theme(text=element_text(size=24))
-
-# Coverage for data with hidden covariates and inverse-gammma prior
-ggplot(
-  subset(aggregate, (Z_additive == TRUE)&(Z_multiplicative==FALSE)&(RndEffects=="A_IG")),
-  aes(x=Variable, y=coverage)
-) +
-  geom_boxplot(aes(col=Variable)) + guides(color=FALSE) +
-  coord_cartesian(ylim=c(0.5,1)) +
-  geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="Nodal Variation, Inverse-Gamma",
-       x="", y="Coverage") +
-  theme(text=element_text(size=24))
-
-# Coverage for data with hidden covariates and half-cauchy prior
-ggplot(
-  subset(aggregate, (Z_additive == TRUE)&(Z_multiplicative==FALSE)&(RndEffects=="A_HC")),
-  aes(x=Variable, y=coverage)
-) +
-  geom_boxplot(aes(col=Variable)) + guides(color=FALSE) +
-  coord_cartesian(ylim=c(0.5,1)) +
-  geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="Nodal Variation, Half-Cauchy",
-       x="", y="Coverage") +
-  theme(text=element_text(size=24))
-
-#######################################################33
-
-# Coverage for when there is no nodal variation
-ggplot(
-  subset(aggregate, (Z_additive == FALSE)&(RndEffects!="None")),
+  subset(aggregate, (Z_additive == FALSE)&(RndEffects!="None")&(projected==FALSE)),
   aes(x=Var, y=coverage)
   ) +
-  geom_boxplot(aes(col=RndEffects)) + #guides(color=FALSE) +
+  geom_boxplot(aes(col=RndEffects)) + guides(color=FALSE) +
   coord_cartesian(ylim=c(0.5,1)) +
   geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="No Nodal Variation",
+  labs(title="Prior Comparison: No Nodal Variation",
        x="", y="Coverage") +
-  theme(text=element_text(size=24))
+  theme(text=element_text(size=24)) +
+  scale_color_manual(values=brewer.pal(10,"Paired")[c(1,3)])
 
 
-# Coverage for when there is nodal variation
+# Coverage for when there is nodal variation - priors
 ggplot(
-  subset(aggregate, (Z_additive == TRUE)&(RndEffects!="None")),
+  subset(aggregate, (Z_additive == TRUE)&(RndEffects!="None")&(projected==FALSE)),
   aes(x=Var, y=coverage)
   ) +
-  geom_boxplot(aes(col=RndEffects)) + #guides(color=FALSE) +
+  geom_boxplot(aes(col=RndEffects)) + guides(color=FALSE) +
   coord_cartesian(ylim=c(0.5,1)) +
   geom_abline(linetype=2, slope=0, intercept=.9) +
-  labs(title="Nodal Variation",
+  labs(title="Prior Comparison: Nodal Variation",
        x="", y="Coverage") +
-  theme(text=element_text(size=24))
+  theme(text=element_text(size=24)) +
+  scale_color_manual(values=brewer.pal(10,"Paired")[c(1,3)])
+
+# Coverage for when there is no nodal variation - projections
+ggplot(
+  subset(aggregate, (Z_additive == FALSE)&(RndEffects!="None")&(RndEffects=="A_IG")),
+  aes(x=Var, y=coverage)
+  ) +
+  geom_boxplot(aes(col=projected)) + guides(color=FALSE) +
+  coord_cartesian(ylim=c(0.5,1)) +
+  geom_abline(linetype=2, slope=0, intercept=.9) +
+  labs(title="Projection Comparison: No Nodal Variation",
+       x="", y="Coverage") +
+  theme(text=element_text(size=24)) +
+  scale_color_manual(values=brewer.pal(10,"Paired")[c(1,5)])
+
+
+# Coverage for when there is nodal variation - projections
+ggplot(
+  subset(aggregate, (Z_additive == TRUE)&(RndEffects!="None")&(RndEffects=="A_IG")),
+  aes(x=Var, y=coverage)
+  ) +
+  geom_boxplot(aes(col=projected)) + guides(color=FALSE) +
+  coord_cartesian(ylim=c(0.5,1)) +
+  geom_abline(linetype=2, slope=0, intercept=.9) +
+  labs(title="Projection Comparison: Nodal Variation",
+       x="", y="Coverage") +
+  theme(text=element_text(size=24)) +
+  scale_color_manual(values=brewer.pal(10,"Paired")[c(1,5)])
 
 
 
