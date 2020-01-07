40b3941f536c71079bd69879b1eac4d356ccd734
Ian Taylor <ian@iantaylor.xyz>
2020-01-06 22:48:54 -0700

    Long overdue commit and changes to correlated random effects generation

--- Code/projection-test/Generate Random Effects/generate-correlated-random-effects.R
+++ Code/projection-test/Generate Random Effects/generate-correlated-random-effects.R
@@ -1,61 +1,38 @@
 library(MASS)
+library(foreach)
 
 n <- 20
-reps.per.X <- 5
-num.X <- 5
+reps.per.X <- 10
+num.X <- 4
 alpha.list <- seq(0,1,by=.1)
 sigma.list <- 1:5
 
 ########################################
 
-# Raise a matrix to an integer power (inefficient, but okay)
-matpow <- function(M, p) {
-  res <- M
-  for (i in 2:p) {
-    res <- res %*% M
-  }
-  return(res)
-}
-
 # Do the eta-prime transformation
-eta.prime <- function(alpha, sigma, eta, I, P) {
+eta.prime <- function(alpha, sigma, eta, P, P.orth) {
+  n <- length(eta)
   # Divide eta into components
-  eta.orth <- (I - P) %*% eta
+  eta.orth <- P.orth %*% eta
   eta.par <- P %*% eta
   # Reassemble and return
   (sqrt(1-alpha) * eta.orth / sqrt(sum(eta.orth^2)) + 
-      sqrt(alpha) * eta.par / sqrt(sum(eta.par^2))) * sigma
+      sqrt(alpha) * eta.par / sqrt(sum(eta.par^2))) * sigma * n
 }
 
 genX <- function(n) {
   #### Generate design matrix X for dyad covariates
   # Start empty
-  Xd <- array(0, dim=c(n,n,4) )
-  # Fill first covariate with binary indicator of shared class
-  groups <- sample(0:1, n, TRUE) * 2 - 1
-  if (all(groups == 1) || all(groups == -1)) {
-    k <- sample(1:n, 1)[1]
-    groups[k] <- -groups[k]
-  }
-  Xd[,,1] <- outer(groups, groups, "*")
-  # Fill second covariate with "distances" between normally distributed coordinates
-  locations <- rnorm(n=n, mean=0, sd=1)
-  dist <- function(x,y) {
-    abs(x - y)
-  }
-  Xd[,,2] <- outer(locations, locations, "dist")
+  Xd <- array(0, dim=c(n,n,3) )
+  # Fill first covariate with a random row effect
+  Xd[,,1] <- matrix(rnorm(n=n, mean=0, sd=1), nrow=n, ncol=n)
+  # Fill the second covariate with an "almost" row covariate
+  Xd[,,2] <- matrix(rnorm(n=n, mean=0, sd=1), nrow=n, ncol=n)
+  Xd[,,2] <- Xd[,,2] + matrix(rnorm(n=n^2, mean=0, sd=1/n), nrow=n, ncol=n)
   # Fill third covariate with all random standard normal draws
   Xd[,,3] <- matrix(rnorm(n=n*n, mean=0, sd=1), nrow=n, ncol=n)
-  # Fill fourth covariate with a random row effect
-  Xd[,,4] <- matrix(rnorm(n=n, mean=0, sd=1), nrow=n, ncol=n)
-  # Set all diagonals to NA
-  #for (i in 1:n) {
-  #  for (k in 1:length(beta)) {
-  #    Xd[i,i,k] <- NA
-  #  }
-  #}
   
-  dimnames(Xd)[[3]] <- c("X1","X2","X3","X4")
+  dimnames(Xd)[[3]] <- c("X1","X2","X3")
   
   mX <- apply(Xd,3,c)
   
@@ -64,9 +41,7 @@ genX <- function(n) {
 
 ########################################
 
-results <- NULL
-
-for (gen in 1:num.X) {
+results <- foreach(gen = 1:num.X, .combine="rbind") %do% {
   X <- genX(n)
   
   A <- matrix(0, ncol=n, nrow=n^2)
@@ -83,34 +58,61 @@ for (gen in 1:num.X) {
   I <- diag(n^2)                             #R^(n^2)
   PX <- X %*% solve(t(X) %*% X) %*% t(X)     # The column space of X
   PAB <- AB %*% ginv(t(AB) %*% AB) %*% t(AB) # The column space of AB 
-  PABX <- matpow(PAB %*% PX, 200)            # (C(AB) intersect C(X))
+  # C(PAB %*% X) - the column space of X projected onto AB.
+  PABX <- (PAB %*% X) %*% ginv(t(X) %*% PAB %*% X) %*% (t(X) %*% PAB)
+  # Attempting to do a "weighted" projection
+  PABXw <- (PAB %*% X) %*% diag(1 / diag(t(X) %*% X)) %*% (t(X) %*% PAB)
   
-  for (rep in 1:reps.per.X) {
+  foreach(rep = 1:reps.per.X, .combine="rbind") %do% {
     eta <- AB %*% rnorm(2*n)
     
-    for (alpha in alpha.list) {
-      for (sigma in sigma.list) {
-        eta.0 <- eta.prime(alpha, sigma, eta, I, PX)
-        eta.2 <- eta.prime(alpha, sigma, eta, PAB, PABX)
+    foreach(alpha = alpha.list, .combine="rbind") %do% {
+      foreach(sigma = sigma.list, .combine="rbind") %do% {
+        eta.4 <- eta.prime(alpha, sigma, eta, PABXw, PAB - PABX)
         
-        results <- rbind(results,
-                         list(alpha=alpha, sigma=sigma, rep=rep, Xgen=gen,
-                              propvar.0=sum((PX%*%eta.0)^2)/sum(eta.0^2),
-                              propvar.2=sum((PX%*%eta.2)^2)/sum(eta.2^2),
-                              mag.0=sqrt(sum(eta.0^2)),
-                              mag.2=sqrt(sum(eta.2^2)),
-                              rhoX1.0=cor(X[,2], eta.0),
-                              rhoX2.0=cor(X[,3], eta.0),
-                              rhoX3.0=cor(X[,4], eta.0),
-                              rhoX4.0=cor(X[,5], eta.0),
-                              rhoX1.2=cor(X[,2], eta.2),
-                              rhoX2.2=cor(X[,3], eta.2),
-                              rhoX3.2=cor(X[,4], eta.2),
-                              rhoX4.2=cor(X[,5], eta.2))
-        )
+        data.frame(list(alpha=alpha, sigma=sigma, rep=rep, Xgen=gen,
+                   propvar.4=sum((PX%*%eta.4)^2)/sum(eta.4^2),
+                   cancor.4=cancor(X, eta.4)$cor,
+                   mag.4=sqrt(sum(eta.4^2)),
+                   rhoX1.4=cor(X[,2], eta.4),
+                   rhoX2.4=cor(X[,3], eta.4),
+                   rhoX3.4=cor(X[,4], eta.4)))
+        
       }
     }
   }
+}
+results <- as.data.frame(results)
+
+
+par(mfrow=c(2,2))
+# Canonical correlation
+for (i in 1:num.X) {
+  plot(x=results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'alpha'], 
+       y=results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'cancor.4'],
+       main=paste("Canonical correlation (X #",i,")",sep=""), ylim=c(0,1),
+       xlab=expression(alpha), ylab="Canonical correlation")
+}
+# (Absolute) Correlation with the row covariate
+for (i in 1:num.X) {
+  plot(x=results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'alpha'], 
+       y=abs(results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'rhoX1.4']),
+       main=paste("Correlation with row covariate (X #",i,")",sep=""), ylim=c(0,1),
+       xlab=expression(alpha), ylab=expression(abs(rho[1])))
+}
+# (Absolute) Correlation with the almost row covariate
+for (i in 1:num.X) {
+  plot(x=results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'alpha'], 
+       y=abs(results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'rhoX2.4']),
+       main=paste("Correlation with almost row covariate (X #",i,")",sep=""), ylim=c(0,1),
+       xlab=expression(alpha), ylab=expression(abs(rho[2])))
+}
+# (Absolute) Correlation with dyad covariate
+for (i in 1:num.X) {
+  plot(x=results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'alpha'], 
+       y=abs(results[(results$sigma==1)&(results$Xgen==i)&(results$rep==1), 'rhoX3.4']),
+       main=paste("Correlation with unstructured covariate (X #",i,")",sep=""), ylim=c(0,1),
+       xlab=expression(alpha), ylab=expression(abs(rho[3])))
 }
 
 
