004d77b478a09e38483266e20e47600c2b9cd5e0
Ian Taylor <ian.taylor@colostate.edu>
2020-07-02 15:11:20 -0600

    STarted using odds covariate, actually look at Delta now instead of Beta (oops)

--- Eurovision/data-analysis.R
+++ Eurovision/data-analysis.R
@@ -6,28 +6,33 @@ library(amenhs)
 Y <- read.csv("2015data/votes-ranks.csv", row.names=1)
 Xc.gender <- read.csv("2015data/covariate-gender.csv", row.names=1, stringsAsFactors=FALSE)
 Xd.lang <- read.csv("2015data/covariate-language.csv", row.names=1)
+Xc.odds <- read.csv("2015data/covariate-odds.csv", row.names=1)
 
 # Which countries are we restricting the analysis to?
 countries.in.final <- row.names(Xc.gender)[Xc.gender[,"Female"] != "#N/A"]
 
+Xd <- as.array(as.matrix(Xd.lang[countries.in.final, countries.in.final]))
+#Xc <- data.matrix(cbind(
+#  Xc.gender[countries.in.final,],
+#  Xc.odds[countries.in.final,"Median16"]
+#))
+#colnames(Xc) <- c(colnames(Xc.gender), "MedianOdds")
+Xc <- data.matrix(data.frame(MedianOdds=Xc.odds[countries.in.final,"Median16"]))
+
 res.no.re <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
-                   Xcol=data.matrix(Xc.gender[countries.in.final,]), 
-                   Xdyad=as.array(as.matrix(Xd.lang[countries.in.final,countries.in.final])), 
+                   Xcol=Xc, Xdyad=Xd, 
                    family="frn", rvar=FALSE, cvar=FALSE)
 
 res <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
-                   Xcol=data.matrix(Xc.gender[countries.in.final,]), 
-                   Xdyad=as.array(as.matrix(Xd.lang[countries.in.final,countries.in.final])), 
+                   Xcol=Xc, Xdyad=Xd, 
                    family="frn")
 
 res.hc <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
-                   Xcol=data.matrix(Xc.gender[countries.in.final,]), 
-                   Xdyad=as.array(as.matrix(Xd.lang[countries.in.final,countries.in.final])), 
+                   Xcol=Xc, Xdyad=Xd, 
                    family="frn", halfcauchy=TRUE)
 
 res.proj <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
-                   Xcol=data.matrix(Xc.gender[countries.in.final,]), 
-                   Xdyad=as.array(as.matrix(Xd.lang[countries.in.final,countries.in.final])), 
+                   Xcol=Xc, Xdyad=Xd,
                    family="frn", halfcauchy=TRUE, project=TRUE)
 
 # Summarize results (BETA)
