ccd2ac484c80cd16dce3e17b7d61c11977e98589
Ian Taylor <ian.taylor@colostate.edu>
2020-07-07 12:58:47 -0600

    Updated analysis with contiguity covariate (pure dyadic, stat. sig.)

--- Eurovision/data-analysis.R
+++ Eurovision/data-analysis.R
@@ -6,12 +6,18 @@ library(amenhs)
 Y <- read.csv("2015data/votes-ranks.csv", row.names=1)
 Xc.gender <- read.csv("2015data/covariate-gender.csv", row.names=1, stringsAsFactors=FALSE)
 Xd.lang <- read.csv("2015data/covariate-language.csv", row.names=1)
+Xd.contig <- read.csv("2015data/covariate-contig.csv", row.names=1)
 Xc.odds <- read.csv("2015data/covariate-odds.csv", row.names=1)
 
 # Which countries are we restricting the analysis to?
 countries.in.final <- row.names(Xc.gender)[Xc.gender[,"Female"] != "#N/A"]
 
-Xd <- as.array(as.matrix(Xd.lang[countries.in.final, countries.in.final]))
+Xd <- array(
+  c(as.matrix(Xd.lang[countries.in.final, countries.in.final]),
+    as.matrix(Xd.contig[countries.in.final, countries.in.final])),
+  dim=c(length(countries.in.final), length(countries.in.final), 2)
+)
+dimnames(Xd)[[3]] <- list("CommonLang", "Contig")
 #Xc <- data.matrix(cbind(
 #  Xc.gender[countries.in.final,],
 #  Xc.odds[countries.in.final,"Median16"]
@@ -21,19 +27,23 @@ Xc <- data.matrix(data.frame(MedianOdds=Xc.odds[countries.in.final,"Median16"]))
 
 res.no.re <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
                    Xcol=Xc, Xdyad=Xd, 
-                   family="frn", rvar=FALSE, cvar=FALSE, dcor=FALSE)
+                   family="frn", rvar=FALSE, cvar=FALSE, dcor=FALSE,
+                   plot=FALSE, gof=FALSE)
 
 res <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
                    Xcol=Xc, Xdyad=Xd, 
-                   family="frn", dcor=FALSE)
+                   family="frn", dcor=FALSE,
+                   plot=FALSE, gof=FALSE)
 
 res.hc <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
                    Xcol=Xc, Xdyad=Xd, 
-                   family="frn", halfcauchy=TRUE, dcor=FALSE)
+                   family="frn", halfcauchy=TRUE, dcor=FALSE,
+                   plot=FALSE, gof=FALSE)
 
 res.proj <- amenhs::ame(Y=as.matrix(Y[countries.in.final,countries.in.final]), 
                    Xcol=Xc, Xdyad=Xd,
-                   family="frn", halfcauchy=TRUE, project=TRUE, dcor=FALSE)
+                   family="frn", halfcauchy=TRUE, project=TRUE, dcor=FALSE,
+                   plot=FALSE, gof=FALSE)
 
 # Summarize results (BETA)
 colMeans(res.no.re$BETA)
