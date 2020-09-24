61d71317c2bd9e14030df44bd4ca6ceb17f7e622
Ian Taylor <ian.taylor@colostate.edu>
2020-09-24 13:55:23 -0600

    Better output and finished summary script

--- Code/projection-test/Large Changes with Projection/summarize.R
+++ Code/projection-test/Large Changes with Projection/summarize.R
@@ -15,22 +15,36 @@ allout <- foreach(f=list.files(outputfolder), .combine="rbind.fill") %do% {
 # 1 column covariate
 out.pc1 <- allout[allout$pc==1,]
 boxplot(relmove_a_proj ~ pr + corr, 
-        data=aggregate(relmove_a_proj ~ pr + corr + run, out.pc1, mean))
+        data=aggregate(relmove_a_proj ~ pr + corr + run, out.pc1, mean),
+        main="Row Random Effect Movement: pc=1")
 
 # Look at the movement in column effects, limiting to cases where there were 5
 # row covariates
 out.pr5 <- allout[allout$pr == 5,]
 boxplot(relmove_b_proj ~ pc + corr, 
-        data=aggregate(relmove_b_proj ~ pc + corr + run, out.pr5, mean))
+        data=aggregate(relmove_b_proj ~ pc + corr + run, out.pr5, mean),
+        main="Column Random Effect Movement: pr=5")
 
 # Second, how much do the estimates of covariate effects change with/without
 # projections?
+
+# Look at movement in first row covariate, limiting to cases where there was only
+# 1 column covariate
 out.pc1$beta_row1_ciwidth <- out.pc1$beta_row1_95q - out.pc1$beta_row1_5q
 out.pc1$delta_row1_ciwidth <- out.pc1$delta_row1_95q - out.pc1$delta_row1_5q
 out.pc1$row1_proj_cishrink <- out.pc1$delta_row1_ciwidth / out.pc1$beta_row1_ciwidth
 boxplot(row1_proj_cishrink ~ pr + corr,
-        data=aggregate(row1_proj_cishrink ~ pr + corr + run, out.pc1, mean))
+        data=aggregate(row1_proj_cishrink ~ pr + corr + run, out.pc1, mean),
+        ylim=c(0,1),
+        main="Row CI Shrinkage: pc=1")
 
-# Look at movement in first row covariate, limiting to cases where there was
-# Only 1 column covariate
-out.pr5$beta_col1_ciwidth <- out.pr5$beta_col1_95q - out.pr5$beta_col1
+# Look at movement in first column covariate, limiting to cases where there were
+# 5 row covariates
+out.pr5$beta_col1_ciwidth <- out.pr5$beta_col1_95q - out.pr5$beta_col1_5q
+out.pr5$delta_col1_ciwidth <- out.pr5$delta_col1_95q - out.pr5$delta_col1_5q
+out.pr5$col1_proj_cishrink <- out.pr5$delta_col1_ciwidth / out.pr5$beta_col1_ciwidth
+boxplot(col1_proj_cishrink ~ pc + corr,
+        data=aggregate(col1_proj_cishrink ~ pc + corr + run, out.pr5, mean),
+        ylim=c(0,1),
+        main="Column CI Shrinkage: pr=5")
+
