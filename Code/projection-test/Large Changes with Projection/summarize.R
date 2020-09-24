54a6742d8d7f717888e6d171acac37acb7f550fb
Ian Taylor <ian.taylor@colostate.edu>
2020-09-24 14:23:41 -0600

    Summary of movement of variance parameter estimates

--- Code/projection-test/Large Changes with Projection/summarize.R
+++ Code/projection-test/Large Changes with Projection/summarize.R
@@ -48,3 +48,28 @@ boxplot(col1_proj_cishrink ~ pc + corr,
         ylim=c(0,1),
         main="Column CI Shrinkage: pr=5")
 
+# Third, Why don't the CI's shrink much for 1 covariate, high correlation?
+# Hypothesis: they're already pretty small b/c of the tendency for the covariate
+# to be able to absorb the unobserved effect
+boxplot(beta_row1_ciwidth ~ pr + corr,
+        data=aggregate(beta_row1_ciwidth ~ pr + corr + run, out.pc1, mean),
+        main="Row unprojected CI width: pc=1")
+boxplot(beta_col1_ciwidth ~ pc + corr,
+        data=aggregate(beta_col1_ciwidth ~ pc + corr + run, out.pr5, mean),
+        main="Column unprojected CI width: pr=5")
+
+# Fourth: how much do estimates of the variance parameters change?
+
+# Look at sigma_a, with pc fixed = 1.
+out.pc1$sigma_a_ciwidth <- out.pc1$sigma_a_95q <- out.pc1$sigma_a_5q
+out.pc1$sigma_a_proj_ciwidth <- out.pc1$sigma_a_proj_95q <- out.pc1$sigma_a_proj_5q
+out.pc1$sigma_a_cishrink <- out.pc1$sigma_a_proj_ciwidth / out.pc1$sigma_a_ciwidth
+out.pc1$sigma_a_meanshrink <- out.pc1$sigma_a_proj_mean / out.pc1$sigma_a_mean
+boxplot(sigma_a_meanshrink ~ pr + corr,
+        data=aggregate(sigma_a_meanshrink ~ pr + corr + run, out.pc1, mean),
+        main="Shrinkage of Mean estimates of sigma_a^2: pc=1",
+        ylim=c(0,1))
+boxplot(sigma_a_cishrink ~ pr + corr,
+        data=aggregate(sigma_a_cishrink ~ pr + corr + run, out.pc1, mean),
+        main="Shrinkage of CI of sigma_a^2: pc=1",
+        ylim=c(0,1))
