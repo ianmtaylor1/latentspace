0bbeab96f4536bcd6aa280b026b9d7452576528b
Ian Taylor <ian.taylor@colostate.edu>
2021-05-26 10:30:03 -0600

    Updating projection coverage plots to cover delta.

--- Code/projection-test/Simulation for Paper/create-plots-and-summary.R
+++ Code/projection-test/Simulation for Paper/create-plots-and-summary.R
@@ -18,7 +18,7 @@ vars <- c("beta_intercept", "beta_row1", "beta_row2", "beta_dyad1", "beta_dyad2"
           "delta_intercept", "delta_row1", "delta_row2", "delta_dyad1", "delta_dyad2")
 for (var in vars) {
   covered <- paste0(var, "_covered")
-  truth <- paste0(var, "_true")
+  truth <- gsub("beta", "delta", paste0(var, "_true"))
   ci.low <- paste0(var, "_5q")
   ci.high <- paste0(var, "_95q")
   results[,covered] <- 1 * ((results[,truth] >= results[,ci.low]) & (results[,truth] <= results[,ci.high]))
@@ -27,7 +27,7 @@ for (var in vars) {
 no_re_vars <- c("beta_intercept", "beta_row1", "beta_row2", "beta_dyad1", "beta_dyad2")
 for (var in no_re_vars) {
   covered <- paste0("no_re_", var, "_covered")
-  truth <- paste0(var, "_true")
+  truth <- gsub("beta", "delta", paste0(var, "_true"))
   ci.low <- paste0("no_re_", var, "_5q")
   ci.high <- paste0("no_re_", var, "_95q")
   results[,covered] <- 1 * ((results[,truth] >= results[,ci.low]) & (results[,truth] <= results[,ci.high]))
